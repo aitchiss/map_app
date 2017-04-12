704e7982967ecff39f153173af65079bf28a0f87
Suzanne <suzanne.aitchison@gmail.com>
2017-04-12 11:42:29 +0100

    persists last selection info

--- public/app.js
+++ public/app.js
@@ -1,6 +1,13 @@
 app = function(){
   var countryList = document.querySelector('#country-list')
   var url = 'https://restcountries.eu/rest/v2'
+  var selectionJson = localStorage.getItem('selection')
+  console.log(selectionJson)
+  if (selectionJson){
+    var country = JSON.parse(selectionJson)[0]
+    populateCountryInfo(country)
+  }
+  
 
   makeRequest(url, requestComplete) 
 
@@ -9,25 +16,34 @@ app = function(){
 
 var handleCountryListChange = function(){
   var selectedCountry = this.value
+
   var url = 'https://restcountries.eu/rest/v2/name/' + selectedCountry + '?fullText=true'
   makeRequest(url, requestCompleteSingleCountry) 
 }
 
 var requestCompleteSingleCountry = function(){
+  var jsonString = this.responseText
+  localStorage.setItem('selection', jsonString)
+
+  var country = JSON.parse(jsonString)[0]
+  populateCountryInfo(country)
+}
+
+var populateCountryInfo = function(country){
   var name = document.createElement('p')
   var population = document.createElement('p')
   var capital = document.createElement('p')
-  var jsonString = this.responseText
-  var country = JSON.parse(jsonString)[0]
+
   var div = document.querySelector('#country-info')
 
   name.innerText = 'Name: ' + country.name
   population.innerText = 'Population: ' + country.population
   capital.innerText = 'Capital: ' + country.capital
-  
+
   div.appendChild(name)
   div.appendChild(population)
   div.appendChild(capital)
+
 }
 
 var requestComplete = function(){
