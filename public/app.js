c590340a546624bbb8023f200c4a47a96a72f9e9
Suzanne <suzanne.aitchison@gmail.com>
2017-04-12 11:31:28 +0100

    selecting a country displays country data

--- public/app.js
+++ public/app.js
@@ -3,6 +3,31 @@ app = function(){
   var url = 'https://restcountries.eu/rest/v2'
 
   makeRequest(url, requestComplete) 
+
+  countryList.onchange = handleCountryListChange
+}
+
+var handleCountryListChange = function(){
+  var selectedCountry = this.value
+  var url = 'https://restcountries.eu/rest/v2/name/' + selectedCountry + '?fullText=true'
+  makeRequest(url, requestCompleteSingleCountry) 
+}
+
+var requestCompleteSingleCountry = function(){
+  var name = document.createElement('p')
+  var population = document.createElement('p')
+  var capital = document.createElement('p')
+  var jsonString = this.responseText
+  var country = JSON.parse(jsonString)[0]
+  var div = document.querySelector('#country-info')
+
+  name.innerText = 'Name: ' + country.name
+  population.innerText = 'Population: ' + country.population
+  capital.innerText = 'Capital: ' + country.capital
+  
+  div.appendChild(name)
+  div.appendChild(population)
+  div.appendChild(capital)
 }
 
 var requestComplete = function(){
